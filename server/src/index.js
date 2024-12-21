a7598558936abb492cca6ab48f3b6220952c612c
Mattique <mr.unknown5264@gmail.com>
2024-12-21 18:19:52 +0500

    change in index.js file

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: `${process.env.CLIENT_URL}`, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
